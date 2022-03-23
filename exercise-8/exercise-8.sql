c1a6504d0ab8b7948261d3a0c6b394cfb68279ff
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-23 15:51:33 -0700

    passing test for exercise 8

--- exercise-8/exercise-8.sql
+++ exercise-8/exercise-8.sql
@@ -1,2 +1,26 @@
 -- rank of film category by times rented
 -- include the name, count, and rank
+SELECT 
+name,
+COUNT(rental_date),
+RANK() OVER (ORDER BY COUNT(rental_date) DESC)
+FROM 
+rental
+LEFT JOIN
+inventory
+ON
+inventory.inventory_id = rental.inventory_id
+LEFT JOIN 
+film
+ON
+film.film_id = inventory.film_id
+LEFT JOIN 
+film_category
+ON
+film.film_id = film_category.film_id
+LEFT JOIN 
+category
+ON
+category.category_id = film_category.category_id
+GROUP BY
+name
